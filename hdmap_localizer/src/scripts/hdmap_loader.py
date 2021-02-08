68f7ce6217163a4993aeb69749231f3845935b64
plemeri <taehoon1018@postech.ac.kr>
2021-02-08 12:20:42 +0000

    hdmap

---
+++ hdmap_localizer/src/scripts/hdmap_loader.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import os
+import pickle
+from numpy.lib.function_base import interp
+import shapefile as sp
+import tqdm
+
+from easydict import EasyDict as ed
+
+from std_msgs.msg import ColorRGBA
+from geodesy.utm import fromLatLong
+from geometry_msgs.msg import PoseStamped, Vector3, Pose, Quaternion, Point
+from sensor_msgs.msg import NavSatFix, Imu
+from visualization_msgs.msg import MarkerArray, Marker
+from hdmap_msgs.msg import HDmap
+import rospy
+
+LINK_TYPE = {'1': 'intersection',
+             '2': 'tollgate_highpass',
+             '3': 'tollgate',
+             '4': 'bus_only',
+             '5': 'switchable_way',
+             '6': 'general',
+             '7': 'to_rest_area',
+             '8': 'in_rest_area',
+             '9': 'out_rest_area',
+             '10': 'to_rest_area',
+             '11': 'in_rest_area',
+             '12': 'out_rest_area',
+             '99': 'etc'}
+
+LINE_TYPE = {'111': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 0.3},
+             '112': {'type': Marker.LINE_LIST, 'color': 'y', 'width': 0.3},
+             '113': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 0.3},
+             '114': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 0.3},
+             '121': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 1.0},
+             '122': {'type': Marker.LINE_LIST, 'color': 'y', 'width': 1.0},
+             '123': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 1.0},
+             '124': {'type': Marker.LINE_STRIP,  'color': 'y', 'width': 1.0},
+
+             '211': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 0.3},
+             '212': {'type': Marker.LINE_LIST, 'color': 'w', 'width': 0.3},
+             '213': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 0.3},
+             '214': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 0.3},
+             '221': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 1.0},
+             '222': {'type': Marker.LINE_LIST, 'color': 'w', 'width': 1.0},
+             '223': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 1.0},
+             '224': {'type': Marker.LINE_STRIP,  'color': 'w', 'width': 1.0},
+
+             '311': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 0.3},
+             '312': {'type': Marker.LINE_LIST, 'color': 'b', 'width': 0.3},
+             '313': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 0.3},
+             '314': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 0.3},
+             '321': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 1.0},
+             '322': {'type': Marker.LINE_LIST, 'color': 'b', 'width': 1.0},
+             '323': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 1.0},
+             '324': {'type': Marker.LINE_STRIP,  'color': 'b', 'width': 1.0},
+
+             '999': {'type': Marker.LINE_STRIP,  'color': 'g', 'width': 0.3},
+             '': {'type': Marker.LINE_STRIP,  'color': 'g', 'width': 0.3},
+             }
+
+LINE_KIND = {'501': 'center_line',
+             '5011': 'switchable_lane',
+             '502': 'u_turn',
+             '503': 'line',
+             '504': 'bus_only',
+             '505': 'edge',
+             '506': 'no_lane_change',
+             '515': 'no_parking',
+             '525': 'guide_line',
+             '530': 'stop_line',
+             '531': 'safe_area',
+             '535': 'bicycle_road',
+             '599': 'etc'}
+
+class hdmap_loader:
+    def __init__(self, map_dir, type, center=None):
+        self.map_dir = map_dir
+        self.type = type
+
+        center = fromLatLong(*center)
+        self.center = center
+
+        self.read_map()
+        self.hdmap_vis()
+
+    def read_map(self):
+        shp_file = os.path.join(self.map_dir, self.type + '.shp')
+        dbf_file = os.path.join(self.map_dir, self.type + '.dbf')
+        shx_file = os.path.join(self.map_dir, self.type + '.shx')
+        pkl_file = os.path.join(self.map_dir, self.type + '.pkl')
+
+        shp = open(shp_file, 'rb')
+        dbf = open(dbf_file, 'rb')
+        shx = open(shx_file, 'rb')
+        
+        shaperecords = sp.Reader(dbf=dbf, shx=shx, shp=shp).shapeRecords()
+
+        self.shaperecords = []
+
+        if os.path.isfile(pkl_file) is True:
+            self.load_map_data(pkl_file)
+            rospy.logdebug('pre loaded map found')
+
+        else:
+            for shaperecord in tqdm.tqdm(shaperecords, desc='loading map data - ' + self.type):
+                shape = shaperecord.shape
+                record = shaperecord.record
+
+                longitude = [i[0] for i in shape.points]
+                latitude  = [i[1] for i in shape.points]
+                altitude  = shape.z
+
+                points = [[i.easting  - self.center.easting, 
+                           i.northing - self.center.northing, 
+                           i.altitude - self.center.altitude] for i in map(fromLatLong, latitude, longitude, altitude)]
+                center = np.mean(points, axis=0).tolist()
+                
+                self.shaperecords.append({'shape': {'points': points, 'center': center}, 'record': record.as_dict()})
+            self.save_map_data(pkl_file)
+        self.shaperecords = [ed(i) for i in self.shaperecords]
+
+        rospy.logdebug('hdmap data loaded')
+
+    def save_map_data(self, pkl_file):
+        with open(pkl_file, 'wb') as f:
+            pickle.dump(self.shaperecords, f)
+
+    def load_map_data(self, pkl_file):
+        with open(pkl_file, 'rb') as f:
+             self.shaperecords = pickle.load(f)
+    
+    def make_marker(self, shaperecord, id, color=None):
+        shape = shaperecord.shape
+        record = shaperecord.record
+
+        marker = Marker()
+        marker.header.stamp = rospy.Time.now()
+        marker.header.frame_id = "map"
+        
+        marker.id = id
+        marker.action = Marker.ADD
+
+        if color is None:
+            if self.type == 'link':
+                marker.type = Marker.LINE_STRIP
+                marker.scale.x = 0.3
+                marker.color = ColorRGBA(0.0, 1.0, 0.0, 1.0)
+
+            elif self.type == 'surfacelinemark':
+                line_type = LINE_TYPE[record.Type]
+                marker.type = line_type['type']
+                marker.scale.x = line_type['width']
+                marker.color = ColorRGBA(*((1.0, 1.0, 1.0, 1.0) if line_type['color'] == 'w' else
+                                        (1.0, 1.0, 0.0, 1.0) if line_type['color'] == 'y' else
+                                        (0.0, 0.0, 1.0, 1.0) if line_type['color'] == 'b' else
+                                        (0.8, 0.6, 0.8, 1.0)))
+            else:
+                marker.type = Marker.LINE_LIST
+                marker.scale.x = 0.1
+                marker.color = ColorRGBA(0.5, 0.5, 0.5, 0.5)
+        else:
+            marker.type = Marker.LINE_STRIP
+            marker.scale.x = 1.0
+            marker.color = ColorRGBA(*color)
+
+        point_list = []
+        for point in shape.points:
+            point_list.append(Point(point[0], point[1], point[2]))
+
+        if marker.type == Marker.LINE_LIST and len(point_list) %2 !=0:
+            point_list.append(Point(point[0], point[1], point[2]))
+        marker.points = point_list
+        marker.pose.position = Point(0, 0, 0)
+        marker.pose.orientation = Quaternion(0, 0, 0, 1)
+
+        return marker
+
+    def make_markerarray(self):
+        markerarray = []
+        id = 0
+        for shaperecord in self.shaperecords:
+            markerarray.append(self.make_marker(shaperecord, id))
+            id += 1
+        return MarkerArray(markerarray)
+
+    def hdmap_vis(self):
+        self.pub = rospy.Publisher("/hdmap/" + self.type, MarkerArray, queue_size=10, latch=True)
+        self.pub.publish(self.make_markerarray())
+
+if __name__ == "__main__":
+    import time
+    rospy.init_node('map_vis', anonymous=True)
+    root_dir = os.path.join(os.path.split(__file__)[0], './map_data')  
+
+    rospy.spin()
---
+++ hdmap_localizer/src/scripts/hdmap_localizer.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from logging import currentframe
+
+from numpy import linalg
+import rospy
+import csv
+import numpy as np
+import os
+from std_msgs.msg import ColorRGBA, Bool
+from geometry_msgs.msg import PoseStamped, Vector3
+from visualization_msgs.msg import Marker, MarkerArray
+import tqdm
+from hdmap_msgs.msg import HDmap
+
+from hdmap_loader import hdmap_loader
+
+class hdmap_localizer:
+    def __init__(self, map_dir, link_type, surf_type, center, candid_num=100, thresh=2.0):
+        self.map_dir = map_dir
+        self.link_type = link_type
+        self.surf_type = surf_type
+        self.candid_num = candid_num
+        self.thresh = thresh
+
+        self.link_map = hdmap_loader(map_dir, link_type, center)
+        self.surf_map = hdmap_loader(map_dir, surf_type, center)
+
+        self.link_id = [i.record.ID for i in self.link_map.shaperecords]
+        self.r_link_id = [i.record.R_LinkID for i in self.link_map.shaperecords]
+        self.l_link_id = [i.record.L_LinKID for i in self.link_map.shaperecords]
+        self.from_node_id = [i.record.FromNodeID for i in self.link_map.shaperecords]
+        self.to_node_id = [i.record.ToNodeID for i in self.link_map.shaperecords]
+
+        self.r_surf_id = [i.record.R_linkID for i in self.surf_map.shaperecords]
+        self.l_surf_id = [i.record.L_linkID for i in self.surf_map.shaperecords]
+
+        self.stop_shaperecords = [i for i in self.surf_map.shaperecords if i.record.Kind == '530']
+        self.link_stop = self.link_stop_lane(0.2)
+        
+        self.center_shaperecords = [i for i in self.surf_map.shaperecords if i.record.Kind == '501']
+        self.border_shaperecords = [i for i in self.surf_map.shaperecords if i.record.Kind == '505']
+
+        self.centers = [i.shape.center for i in self.link_map.shaperecords]
+        self.prev_link = None
+        self.reliable = False
+
+        self.pub_vis = rospy.Publisher('/current_link_vis', MarkerArray, queue_size=10)
+        self.pub_hdmap = rospy.Publisher('/hdmap/lane_info', HDmap, queue_size=10)
+    
+    def get_link(self, point):
+        # coarse candidates
+        d = (np.array(self.centers) - point) ** 2
+        d = np.sqrt(d.sum(axis=1))
+        c_candid_idx = np.argsort(d)[:self.candid_num]
+
+        # fine candidates
+        md = []
+        for idx in c_candid_idx:
+            d = (np.array(self.link_map.shaperecords[idx].shape.points) - point) ** 2
+            d = np.sqrt(d.sum(axis=1))
+            md.append(d.min())
+        f_candid_idx = np.where(np.array(md) < self.thresh)[0]
+        f_candid_idx = c_candid_idx[f_candid_idx]
+
+        # sort candidates
+        if len(f_candid_idx) == 1:
+            self.reliable = True
+            return f_candid_idx[0]
+        else:
+            if self.prev_link not in f_candid_idx:
+                self.reliable = False
+                if len(f_candid_idx) > 0:
+                    self.prev_link = f_candid_idx.min()
+                else:
+                    self.prev_link = None
+            return self.prev_link
+    
+    def link_stop_lane(self, thresh, r=2):
+        rospy.loginfo('LINKING STOP LANES')
+
+        # initialize
+        end_link_points = np.array([i.shape.points[-1] for i in self.link_map.shaperecords])
+        stop_points = [i.shape.points for i in self.stop_shaperecords]
+        link_stop = [None] * len(self.link_map.shaperecords)
+        for i, stop in enumerate(stop_points):
+            stop = np.stack([stop] * len(end_link_points), axis=1)
+            d = np.linalg.norm(stop - end_link_points, axis=-1).min(0)
+            links = np.where(d < thresh)[0].tolist()
+            for link in links:
+                if link_stop[link] is None:
+                    link_stop[link] = i
+                else:
+                    rospy.logwarn('Duplicate stop lane detected')
+        
+        # recursive
+        for rec in range(r):
+            for i, shaperecord in enumerate(self.link_map.shaperecords):
+                if link_stop[i] is not None:
+                    continue
+                else:
+                    next_idxs = np.where(np.array(self.from_node_id) == self.to_node_id[i])[0]
+                    
+                    stop_candid = list()
+                    for next_idx in next_idxs:
+                        if link_stop[next_idx] is not None:
+                            stop_candid.append(link_stop[next_idx])
+
+                        stop_idxs = np.unique(stop_candid)
+                        if len(stop_idxs) == 1:
+                            link_stop[i] = stop_idxs[0]
+        return link_stop
+
+    def link_description(self, current_link, id=0, use_reliable=None, color=None):
+        if use_reliable is None and color is None:
+            color = (0.5, 0.5, 0.5, 0.5)
+        elif use_reliable is not None:
+            color = (0.0, 0.0, 1.0, 1.0) if self.reliable is True else (1.0, 0.0, 0.0, 1.0)
+        elif use_reliable is not None and color is not None:
+            raise AttributeError('In function \'link_description\', use between use_reliable or color')
+        shaperecord = self.link_map.shaperecords[current_link]
+        marker = self.link_map.make_marker(shaperecord, id, color)
+        return marker
+
+    def pose_callback(self, msg):
+        markerarray = []
+        lane_info = HDmap()
+        lane_info.header.stamp = rospy.Time.now()
+        lane_info.header.frame_id = 'map'
+        lane_info.reliable = Bool(data=self.reliable)
+
+        point = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+        current_link = self.get_link(point)
+    
+        id = 0
+        if current_link is not None:
+            current_link_id = self.link_id[current_link]
+
+            # current link
+            current_lane_marker = self.link_description(current_link, id, use_reliable=True)
+            lane_info.current_link
+            markerarray.append(current_lane_marker)
+            id += 1
+
+            # left / right link
+            left_link = self.link_id.index(self.l_link_id[current_link]) if self.l_link_id[current_link] != '' else None
+            right_link = self.link_id.index(self.r_link_id[current_link]) if self.r_link_id[current_link] != '' else None
+
+            if left_link is not None:
+                left_link_marker = self.link_description(left_link, id, color=(0.0, 0.6, 1.0, 1.0))
+                lane_info.left_link = left_link_marker
+                markerarray.append(left_link_marker)
+
+                id += 1
+            if right_link is not None:
+                right_link_marker = self.link_description(right_link, id, color=(1.0, 0.6, 0.0, 1.0))
+                lane_info.right_link = right_link_marker
+                markerarray.append(right_link_marker)
+                id += 1
+
+            # stop line
+            current_stop_line = self.link_stop[current_link]
+            if current_stop_line is not None:
+                stop_shaperecord = self.stop_shaperecords[current_stop_line]
+                points = stop_shaperecord.shape.points
+                dist = np.linalg.norm(np.array(points) - point, axis=1).min()
+                lane_info.stop_dist = dist
+
+                stop_line_marker = self.surf_map.make_marker(stop_shaperecord, id, (0.5, 0.5, 1.0, 1.0))
+                lane_info.stop_line = stop_line_marker
+                markerarray.append(stop_line_marker)
+                id += 1
+
+            # left / right surf
+            if current_link_id in self.l_surf_id:
+                left_surf = self.l_surf_id.index(current_link_id)
+                left_surf_marker = self.surf_map.make_marker(self.surf_map.shaperecords[left_surf], id, (0.5, 0.0, 0.5, 0.5))
+                lane_info.left_line = left_surf_marker
+                markerarray.append(left_surf_marker)
+                id += 1
+
+            if current_link_id in self.r_surf_id:
+                right_surf = self.r_surf_id.index(current_link_id)
+                right_surf_marker = self.surf_map.make_marker(self.surf_map.shaperecords[right_surf], id, (0.5, 0.5, 0.0, 0.5))
+                lane_info.right_line = right_surf_marker
+                markerarray.append(right_surf_marker)
+                id += 1
+
+            self.pub_vis.publish(MarkerArray(markerarray))
+            self.pub_hdmap.publish(lane_info)
+        else:
+            rospy.logwarn('CURRENT LANE NOT FOUND')
+
+if __name__ == '__main__':
+    rospy.init_node('hdmap_localizer')
+    pose_topic = rospy.get_param('~pose_topic')
+    center_latitude = rospy.get_param('~center_latitude')
+    center_longitude = rospy.get_param('~center_longitude')
+    center_altitude = rospy.get_param('~center_altitude')
+    localizer = hdmap_localizer(os.path.join(os.environ['AUTOWARE_DIR'], 'src', 'Data', 'Maps', 'HDmap'), 'link', 'surfacelinemark', (center_latitude, center_longitude, center_altitude))
+    sub = rospy.Subscriber(pose_topic, PoseStamped, localizer.pose_callback)
+    rospy.spin()
+
+
